949b1f1fc2a6e49c14f706e833e69874faa7fa2f
Tomek Stolarczuk <tomstolarczuk@protonmail.com>
2025-10-23 22:42:05 +0200

    feat: add new enums

---
+++ src/Scte35.Net/Model/Enums/DeviceRestrictions.cs
@@ -0,0 +1,11 @@
+using Scte35.Net.Constants;
+
+namespace Scte35.Net.Model.Enums;
+
+public enum DeviceRestrictions
+{
+    Group0 = Scte35Constants.DeviceRestrictionsGroup0,
+    Group1 = Scte35Constants.DeviceRestrictionsGroup1,
+    Group2 = Scte35Constants.DeviceRestrictionsGroup2,
+    None = Scte35Constants.DeviceRestrictionsNone,
+}
